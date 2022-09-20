fcf8fa5a9d4cd44ee786bb574494a1e84eb5ccea
Liliana Leiva <liliana-carmen@hotmail.com>
2022-09-20 19:36:49 -0300

    properties css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,10 @@ import logo from '../../img/logo.png';
 function Navbar() {
   return (
     <div className={styles.container}>
+      <div className={styles.content}>
         <img className={styles.logo} src={logo} alt='logo' />
         <p>Searchbar</p>
+      </div>
     </div>
   )
 }
--- src/components/Properties/Properties.jsx
+++ src/components/Properties/Properties.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import styles from './Properties.module.css';
+import stays from '../../stays.json';
 
 function Properties() {
+  console.log('stays', stays)
+
   return (
     <div className={styles.container}>
-      Properties
+      <h1 className={styles.title}>Stays in Finland</h1>
+      <li className={styles.list}>
+        {
+          stays.map((stay) => {
+            return (
+              <ul className={styles.stay}>
+                <img src={stay.photo} alt='photo' className={styles.photo} />
+                <div className={styles.row}>
+                  <div className={styles.roww}>
+                    {
+                      stay.superHost && <p className={styles.superhost}>SUPER HOST</p>
+                    }
+                    <p className={styles.type}>{stay.type}</p>
+                  </div>
+                  <p className={styles.rating}>{stay.rating}</p>
+                </div>
+                <p className={styles.description}>{stay.title}</p>
+              </ul>
+            )
+          })
+        }
+      </li>
     </div>
   )
 }
 
-export default Properties
+export default Properties;
