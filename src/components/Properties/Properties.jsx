b7803805dc398c2545b371fcbfdb1a1398117cfa
lilileiva <liliana-carmen@hotmail.com>
2023-01-08 21:50:55 -0300

    search properties and no result screen

--- src/components/Properties/Properties.jsx
+++ src/components/Properties/Properties.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Properties.module.css';
 import stays from '../../stays.json';
 import './Properties.css';
 
-function Properties() {
-  console.log('stays', stays)
+function Properties({ numberGuests }) {
+
+  let properties = []
+  if (numberGuests == "") properties = stays
+  else properties = stays.filter((stay) => stay.maxGuests == numberGuests)
 
   return (
     <div className={styles.container}>
@@ -12,32 +15,38 @@ function Properties() {
         <h1 className={styles.title}>Stays in Finland</h1>
         <p className={styles.staysAmount}>{stays.length}+ stays</p>
       </div>
-      <li className={styles.list}>
         {
-          stays.map((stay) => {
-            return (
-              <ul className={styles.stay}>
-                <img src={stay.photo} alt='photo' className={styles.photo} />
-                <div className={styles.rowSuperhostRating}>
-                  <div className={styles.rowSuperhostType}>
-                    {
-                      stay.superHost && <p className={styles.superhost}>SUPER HOST</p>
-                    }
-                    <p className={styles.type}>{stay.type}</p>
-                  </div>
-                  <div className={styles.ratingContainer}>
-                    <span class="material-symbols-outlined">
-                      star
-                    </span>
-                    <p className={styles.rating}>{stay.rating}</p>
-                  </div>
-                </div>
-                <p className={styles.description}>{stay.title}</p>
-              </ul>
-            )
-          })
+          properties.length == 0
+            ? <div className={styles.noResults}>
+              <p>No results...</p>
+            </div>
+            :  <li className={styles.list}>
+              {
+                  properties.map((stay) => {
+                    return (
+                      <ul className={styles.stay}>
+                        <img src={stay.photo} alt='photo' className={styles.photo} />
+                        <div className={styles.rowSuperhostRating}>
+                          <div className={styles.rowSuperhostType}>
+                            {
+                              stay.superHost && <p className={styles.superhost}>SUPER HOST</p>
+                            }
+                            <p className={styles.type}>{stay.type}</p>
+                          </div>
+                          <div className={styles.ratingContainer}>
+                            <span class="material-symbols-outlined">
+                              star
+                            </span>
+                            <p className={styles.rating}>{stay.rating}</p>
+                          </div>
+                        </div>
+                        <p className={styles.description}>{stay.title}</p>
+                      </ul>
+                    )
+                  })
+              }
+            </li>
         }
-      </li>
     </div>
   )
 }
