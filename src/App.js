ca6acf155f1d1be4aa4bc26454b205d9f5efc719
lilileiva <liliana-carmen@hotmail.com>
2023-01-08 21:50:14 -0300

    searchbar logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Properties from './components/Properties/Properties';
 
 function App() {
+
+  let num
+  const [numberGuests, setNumberGuests] = useState("")
+
+  const handleInputChange = (e) => {
+    if (e.target.value == Number(e.target.value) && e.target.value !== " " && Number(e.target.value) !== NaN) num = e.target.value
+    else e.target.value = ""
+  }
+
+  const handleInputSubmit = (e) => {
+    e.preventDefault()
+    if (num) setNumberGuests(Number(num))
+    else setNumberGuests("")
+  }
+
   return (
     <div className="App">
       <div className="container">
-        <Navbar />
-        <Properties />
+        <Navbar handleInputChange={handleInputChange} handleInputSubmit={handleInputSubmit} />
+        <Properties numberGuests={numberGuests} />
         <Footer />
       </div>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,14 +3,14 @@ import styles from './Navbar.module.css';
 import logo from '../../img/logo.png';
 import Searchbar from '../Searchbar/Searchbar';
 
-function Navbar() {
+function Navbar({ handleInputChange, handleInputSubmit }) {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <img className={styles.logo} src={logo} alt='logo' />
         <div className={styles.row}>
           <p className={styles.location}>Helsinki, Finland</p>
-          <Searchbar />
+          <Searchbar handleInputChange={handleInputChange} handleInputSubmit={handleInputSubmit} />
         </div>
       </div>
     </div>
