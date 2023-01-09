6c6dc9dd6a74499dc8a8a324a303b6495f6ea1ab
lilileiva <liliana-carmen@hotmail.com>
2023-01-08 21:51:19 -0300

    searchbar props

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Searchbar.module.css';
+import stays from '../../stays.json';
 
-function Searchbar() {
+
+function Searchbar({ handleInputChange, handleInputSubmit }) {
   return (
-    <form className={styles.container}>
-      <input onFocus='false' type='text' placeholder='Add guests' />
-      <div className={styles.submit}>
+    <form className={styles.container} onSubmit={(e) => handleInputSubmit(e)}>
+      <input onFocus='false' type='text' placeholder='Add guests' onChange={(e) => handleInputChange(e)} />
+      <div className={styles.submit} onClick={(e) => handleInputSubmit(e)}>
         <span class="material-symbols-outlined">
           search
         </span>
