9bfb9c369c8994b9f79d05c34c65b7bc745540cd
lilileiva <liliana-carmen@hotmail.com>
2023-01-12 23:32:28 -0300

    add cities list in dropmenu and navbar style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import styles from './Navbar.module.css';
 import logo from '../../img/logo.png';
 import Searchbar from '../Searchbar/Searchbar';
+import stays from '../../stays.json';
 
 function Navbar({ handleInputChange, handleInputSubmit, dropdownMenu, changeDropdownState }) {
 
-  // const [dropdownMenu, setDropdownMenu] = useState(false)
-
-  // return (
-  //   <div className={styles.container}>
-  //     <div className={styles.content}>
-  //       <img className={styles.logo} src={logo} alt='logo' />
-  //       <div className={styles.row}>
-  //         <p className={styles.location} onClick={() => setDropdownMenu(true)}>
-  //           Helsinki, Finland
-  //         </p>
-  //         <Searchbar handleInputChange={handleInputChange} handleInputSubmit={handleInputSubmit} />
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
+  let cities = []
+  stays.filter((stay) => cities.push(stay.city))
+  let citiesFiltered = []
+  cities.map((city) => !citiesFiltered.includes(city) ? citiesFiltered.push(city) : null)
 
   return <>
     {
       dropdownMenu ? (
         <div className={styles.container}>
           <div className={styles.dropdown}>
-            <div className={styles.content}>
+            <div className={styles.content}>            
+              <div className={styles.bar}>
+                <div>
+                  <p className={styles.locationBar}>Location</p>
+                  <p className={styles.location} onClick={() => changeDropdownState()}>
+                    Helsinki, Finland
+                  </p>
+                </div>
+                <Searchbar handleInputChange={handleInputChange} handleInputSubmit={handleInputSubmit} />
+              </div>
+              <li>
+                {
+                  citiesFiltered.map((city) => (
+                    <ul>
+                      <p>{city}, Finland</p>
+                    </ul>
+                  ))
+                }
+              </li>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <div className={styles.spaceBetween}>
               <img className={styles.logo} src={logo} alt='logo' />
               <div className={styles.row}>
                 <p className={styles.location} onClick={() => changeDropdownState()}>
@@ -34,18 +49,6 @@ function Navbar({ handleInputChange, handleInputSubmit, dropdownMenu, changeDrop
                 </p>
                 <Searchbar handleInputChange={handleInputChange} handleInputSubmit={handleInputSubmit} />
               </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className={styles.container}>
-          <div className={styles.content}>
-            <img className={styles.logo} src={logo} alt='logo' />
-            <div className={styles.row}>
-              <p className={styles.location} onClick={() => changeDropdownState()}>
-                Helsinki, Finland
-              </p>
-              <Searchbar handleInputChange={handleInputChange} handleInputSubmit={handleInputSubmit} />
             </div>
           </div>
         </div>
